30cc342ea75413547ff932136d733abad6742d53
Nick Ng <ngyewch@gmail.com>
2023-10-21 12:22:11 +0800

    feat: checksum support

---
+++ installer/checksums.go
@@ -0,0 +1,102 @@
+package installer
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io"
+	"os"
+	"strings"
+)
+
+type FileChecksum struct {
+	Checksum []byte
+	Filename string
+}
+
+type FileChecksums struct {
+	Entries []*FileChecksum
+}
+
+func ReadFileChecksumsFromFile(path string) (*FileChecksums, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	return ReadFileChecksums(f)
+}
+
+func ReadFileChecksums(r io.Reader) (*FileChecksums, error) {
+	var fileChecksums FileChecksums
+
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		parts := strings.SplitN(scanner.Text(), "  ", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid format")
+		}
+		checksum, err := hex.DecodeString(parts[0])
+		if err != nil {
+			return nil, err
+		}
+		fileChecksums.Entries = append(fileChecksums.Entries, &FileChecksum{
+			Checksum: checksum,
+			Filename: parts[1],
+		})
+	}
+
+	return &fileChecksums, nil
+}
+
+func (checksums *FileChecksums) GetEntry(filename string) *FileChecksum {
+	for _, entry := range checksums.Entries {
+		if entry.Filename == filename {
+			return entry
+		}
+	}
+	return nil
+}
+
+func CalcFileChecksum(path string, alg string) ([]byte, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	return CalcChecksum(f, alg)
+}
+
+func CalcChecksum(r io.Reader, alg string) ([]byte, error) {
+	var hasher hash.Hash
+	switch alg {
+	case "md5":
+		hasher = md5.New()
+	case "sha1":
+		hasher = sha1.New()
+	case "sha256":
+		hasher = sha256.New()
+	case "sha512":
+		hasher = sha512.New()
+	default:
+		return nil, fmt.Errorf("unsupported algorithm")
+	}
+
+	_, err := io.Copy(hasher, r)
+	if err != nil {
+		return nil, err
+	}
+
+	return hasher.Sum(nil), nil
+}
--- installer/installer.go
+++ installer/installer.go
@@ -1,10 +1,16 @@
 package installer
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/google/go-github/v56/github"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func Install(packageSpec string) error {
@@ -17,12 +23,13 @@ func Install(packageSpec string) error {
 	archClassifier := runtime.GOARCH // TODO always assume runtime.GOARCH?
 	// TODO always assume this format?
 	// TODO always assume .tar.gz extension?
-	downloadFilename := fmt.Sprintf("%s_%s_%s_%s.tar.gz",
+	baseDownloadFilename := fmt.Sprintf("%s_%s_%s_%s",
 		pkgSpec.Project, // TODO always assume filename == projectName?
 		pkgSpec.Version, // TODO always assume no prefix?
 		osClassifier,
 		archClassifier,
 	)
+	downloadFilename := baseDownloadFilename + ".tar.gz"
 	downloadUrl := fmt.Sprintf("https://github.com/%s/%s/releases/download/v%s/%s",
 		pkgSpec.Owner,
 		pkgSpec.Project,
@@ -34,13 +41,103 @@ func Install(packageSpec string) error {
 
 	githubClient := github.NewClient(nil)
 
+	var packageAsset *github.ReleaseAsset
 	release, _, err := githubClient.Repositories.GetReleaseByTag(context.Background(), pkgSpec.Owner, pkgSpec.Project, fmt.Sprintf("v%s", pkgSpec.Version))
 	if err != nil {
 		return err
 	}
+	prefix := baseDownloadFilename + "."
 	for _, asset := range release.Assets {
-		fmt.Printf("%s -> %s\n", *asset.Name, *asset.URL)
+		if strings.HasPrefix(asset.GetName(), prefix) {
+			ext := asset.GetName()[len(baseDownloadFilename):]
+			if (ext == ".tar.gz") || (ext == ".zip") { // TODO other extensions?
+				fmt.Printf("[download] %s -> %s\n", asset.GetName(), asset.GetBrowserDownloadURL())
+				packageAsset = asset
+				break
+			}
+		}
+	}
+
+	var checksumsAsset *github.ReleaseAsset
+	checksumsFilename := fmt.Sprintf("%s_%s_checksums.txt", pkgSpec.Project, pkgSpec.Version)
+	for _, asset := range release.Assets {
+		if asset.GetName() == checksumsFilename {
+			fmt.Printf("[checksums] %s -> %s\n", asset.GetName(), asset.GetBrowserDownloadURL())
+			checksumsAsset = asset
+			break
+		}
+	}
+
+	downloadDirectory := filepath.Join("dist", "downloads", "github.com", pkgSpec.Owner, pkgSpec.Project, pkgSpec.Version)
+
+	if packageAsset != nil {
+		packagePath, err := downloadAsset(packageAsset, downloadDirectory)
+		if err != nil {
+			return err
+		}
+
+		if checksumsAsset != nil {
+			fileChecksumsPath, err := downloadAsset(checksumsAsset, downloadDirectory)
+			if err != nil {
+				return err
+			}
+			fileChecksums, err := ReadFileChecksumsFromFile(fileChecksumsPath)
+			if err != nil {
+				return err
+			}
+			fileChecksumEntry := fileChecksums.GetEntry(packageAsset.GetName())
+			if err != nil {
+				return err
+			}
+			if fileChecksumEntry != nil {
+				checksum, err := CalcFileChecksum(packagePath, "sha512")
+				if err != nil {
+					return err
+				}
+				if !bytes.Equal(checksum, fileChecksumEntry.Checksum) {
+					return fmt.Errorf("checksum mismatch")
+				}
+			}
+		}
 	}
 
 	return nil
+}
+
+func downloadAsset(asset *github.ReleaseAsset, downloadDirectory string) (string, error) {
+	err := os.MkdirAll(downloadDirectory, 0755)
+	if err != nil {
+		return "", err
+	}
+
+	downloadFile := filepath.Join(downloadDirectory, asset.GetName())
+	_, err = os.Stat(downloadFile)
+	if err == nil {
+		fmt.Printf("skipping %s...\n", asset.GetName())
+	} else if !os.IsNotExist(err) {
+		return "", err
+	}
+
+	httpResponse, err := http.Get(asset.GetBrowserDownloadURL())
+	if err != nil {
+		return "", err
+	}
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(httpResponse.Body)
+
+	f, err := os.Create(downloadFile)
+	if err != nil {
+		return "", err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	_, err = io.Copy(f, httpResponse.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return downloadFile, nil
 }
